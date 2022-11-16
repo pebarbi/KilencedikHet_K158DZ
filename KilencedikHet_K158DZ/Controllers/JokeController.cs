b404e84c48b6da5684e242c4cc60285272d46a7f
K158DZ <K158DZ@SZA0030879>
2022-11-16 16:46:34 +0100

    Add project files.

---
+++ KilencedikHet_K158DZ/Controllers/JokeController.cs
@@ -0,0 +1,82 @@
+﻿using KilencedikHet_K158DZ.JokeModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KilencedikHet_K158DZ.Controllers
+{
+    [Route("api/jokes")]
+    [ApiController]
+
+    public class JokeController : ControllerBase
+    {
+        // GET: api/jokes
+        [HttpGet]
+        public IActionResult Get()
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            return Ok(context.Jokes.ToList());
+        }
+
+        //// GET api/jokes/5
+        //[HttpGet("{id}")]
+        //public IActionResult Get(int id)
+        //{
+        //    FunnyDatabaseContext context = new FunnyDatabaseContext();
+        //    var keresettVicc = (from x in context.Jokes
+        //                        where x.JokeSk == id
+        //                        select x).FirstOrDefault();
+        //    return Ok(keresettVicc);
+        //}
+
+        // GET api/jokes/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var keresettVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            if (keresettVicc == null)
+            {
+                return NotFound($"Nincs #{id} azonosítóval vicc");
+            }
+            else
+            {
+                return Ok(keresettVicc);
+            }
+        }
+
+        // POST api/jokes
+        [HttpPost]
+        public void Post([FromBody] Joke újVicc)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(újVicc);
+            context.SaveChanges();
+        }
+
+        // PUT api/<JokeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/jokes/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var törlendőVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendőVicc);
+            context.SaveChanges();
+        }
+    }
+}
