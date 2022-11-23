8305ba50109bceeeea02d6502d1f31072b44d030
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2022-11-23 16:52:51 +0100

    Pascal

---
+++ KilencedikHet_K158DZ/wwwroot/jsgyak.js
@@ -0,0 +1,24 @@
+﻿// JavaScript source code
+
+var faktorialis = n => {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    else {
+        return n * faktorialis(n - 1)
+    }
+};
+var pascal = document.getElementById("pascal");
+var méret = 10;
+for (var s = 0; s <= méret; s++) {
+    var újSor = document.createElement("div");
+    újSor.classList.add("sor");    
+    pascal.appendChild(újSor);
+
+    for (var o = 0; o <= s; o++) {
+        var újElem = document.createElement("div");
+        újElem.innerHTML = faktorialis(s) / (faktorialis(o) * faktorialis(s - o)) 
+        újElem.classList.add("elem");
+        újSor.appendChild(újElem);
+    }
+}
