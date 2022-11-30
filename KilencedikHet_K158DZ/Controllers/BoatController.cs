da8818616d4e4f5b75fc180d01849ba68778a309
Barbara Pecsenye <barbara.pecsenye@stud.uni-corvinus.hu>
2022-11-30 15:55:10 +0100

    gyak11_elsoresz

---
+++ KilencedikHet_K158DZ/Controllers/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using KilencedikHet_K158DZ.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KilencedikHet_K158DZ.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
